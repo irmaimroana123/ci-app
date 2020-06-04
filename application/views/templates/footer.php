803ea568210cd9d5251c77f019561326232519a5
lutfianali128 <63223885+lutfianali128@users.noreply.github.com>
2020-06-04 08:17:31 +0700

    ci-app

---
+++ application/views/templates/footer.php
@@ -0,0 +1,7 @@
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="http://localhost/ci-app/asset/jquery/jquery-3.3.1.slim.min.js"></script>
+    <script src="http://localhost/ci-app/asset/popper/popper.min.js"></script>
+    <script src="http://localhost/ci-app/asset/js/bootstrap.min.js"></script>
+    </body>
+    </html>
